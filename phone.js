10c36c97deb7d0176e33b56502b9d4cdddcaca31
shihab4513 <shihab4513@gmail.com>
2024-06-10 19:59:30 +0600

    show detials modal part is fully completed

--- phone.js
+++ phone.js
@@ -49,7 +49,7 @@ const displayPhones = (phones, isShowAll) => {
                         <h2 class="card-title">${phone.phone_name}</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div class="card-actions justify-center">
-                            <button onclick="handleShowDetails('${phone.slug}}')" class="btn btn-primary">Show Details</button>
+                            <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
                         </div>
                     </div>
         
@@ -88,14 +88,43 @@ const handleShowAll = () => {
 //handle show details
 
 const handleShowDetails = async (id) => {
-    console.log(id);
+
     // load individual phone data
-    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-    const res = await fetch(url);
+    // const url = ;
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
+    const phone = data.data;
+    showPhoneDetails(phone);
 }
+
+const showPhoneDetails = (phone) => {
+    // display the modal
+    console.log(phone);
+    const phoneName = document.getElementById('showDetails-phone-name');
+    phoneName.innerText = phone.name;
+    const showDetailsContainer = document.getElementById('show-details-container');
+    showDetailsContainer.innerHTML = `
+            <img src="${phone.image}" alt="" srcset="">    
+            <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Storage : </span>${phone?.mainFeatures?.storage}  </p>
+            <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Display Size : </span>${phone?.mainFeatures?.displaySize}  </p>
+             <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Chipset : </span>${phone?.mainFeatures?.chipSet}  </p>
+              <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Memory : </span>${phone?.mainFeatures?.memory}  </p>
+               <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Slug : </span>${phone?.slug}  </p>
+                <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Release data : </span>${phone?.releaseDate}  </p>
+                 <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">Brand : </span>${phone?.brand}  </p>
+                
+                   <p class="text-[1.25rem] font-normal text-[#706F6F]"><span class="font-semibold text-[#403F3F]">GPS : </span>${phone?.others?.GPS}  </p>
+    `;
+
+
+    show_details_modal.showModal();
+
+}
+
 // loadPhone();
+
+
 
 
 
